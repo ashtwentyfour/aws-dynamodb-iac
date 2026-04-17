0244031d6e9764e246bc6f87949ab47ccbf7084a
Ashwin Menon <ashtwentyfour@gmail.com>
2026-04-17 19:18:10 -0400

    added ec2

--- main.tf
+++ main.tf
@@ -7,6 +7,31 @@ locals {
   unique_queue_names = toset(var.queue_names)
 }
 
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "example" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
 resource "aws_dynamodb_table" "tables" {
   for_each = local.unique_table_names
 
